c329ba990abddd1cd7297c31dffd3adcd4d256f8
Musafar <musafarvalayankaden@gmail.com>
2026-08-25 19:21:24 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, choice;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
+    printf("4. Divide\n");
+
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1:
+            printf("Result = %d", a + b);
+            break;
+
+        case 2:
+            printf("Result = %d", a - b);
+            break;
+
+        case 3:
+            printf("Result = %d", a * b);
+            break;
+
+        case 4:
+            if (b != 0)
+                printf("Result = %d", a / b);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        default:
+            printf("Invalid choice");
+    }
+
+    return 0;
+}
